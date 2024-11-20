3973a6f7243fc3b92efe2a5f07f1735c78508f3b
Afanasev Dmitry <azh.good@yandex.ru>
2024-11-20 05:44:41 +0300

    Added ExceptionHandlerMiddleware and ResponseModel

---
+++ 4321Afanasev/Models/ResponseModel.cs
@@ -0,0 +1,27 @@
+﻿namespace _4321Afanasev.Models
+{
+
+    public class ResponseModel<T>
+    {
+        public bool Succeeded { get; set; }
+        public string Message { get; set; } = string.Empty; // Значение по умолчанию
+        public List<string> Errors { get; set; } = new List<string>(); // Инициализация пустого списка
+        public T? Data { get; set; } // Допустимость null
+
+        public ResponseModel() { }
+
+        public ResponseModel(T data, string message = "")
+        {
+            Succeeded = true;
+            Message = message ?? string.Empty;
+            Data = data;
+        }
+
+        public ResponseModel(string message)
+        {
+            Succeeded = true;
+            Message = message ?? string.Empty;
+        }
+    }
+
+}
